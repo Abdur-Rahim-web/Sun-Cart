37ca0ddfe0add3d208b078a2b8ac7b30ed53ae9e
Abdur-Rahim-web <abdurrahimar04@gmail.com>
2026-05-01 08:10:47 +0600

    fixd some issue

---
+++ src/app/(main)/products/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductsDetailsPage = () => {
+    return (
+        <div>
+            jghjgfhdfhddfghlluyity
+        </div>
+    );
+};
+
+export default ProductsDetailsPage;
--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -1,47 +1,45 @@
 import { Button, Card } from '@heroui/react';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { FaStar } from 'react-icons/fa';
 
 const ProductCard = ({ product }) => {
-    const { name, rating, price, image } = product
-    return (
-        <div>
-            <Card className="w-full p-4 rounded-2xl shadow-md hover:shadow-xl transition duration-300">
+    const { id, name, rating, price, image } = product;
 
-                
-                <div className="relative w-full h-52 overflow-hidden rounded-xl">
-                    <Image
-                        src={image}
-                        alt={name}
-                        fill
-                        className="object-cover hover:scale-110 transition duration-300"
-                    />
+    return (
+        <Card className="w-full p-4 rounded-2xl shadow-md hover:shadow-xl transition duration-300">
+
+            <div className="relative w-full h-52 overflow-hidden rounded-xl">
+                <Image
+                    src={image}
+                    alt={name}
+                    fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    className="object-cover hover:scale-110 transition duration-300"
+                />
+            </div>
+
+            <div className="mt-4 flex flex-col gap-2">
+                <h3 className="text-lg font-semibold">{name}</h3>
+
+                <p className="font-bold">
+                    Price: <span className="text-orange-500">${price}</span>
+                </p>
+
+                <div className="flex items-center gap-2 text-sm">
+                    <FaStar className="text-yellow-500" />
+                    {rating}
                 </div>
 
-                
-                <div className="mt-4 flex flex-col gap-2">
-
-                    <h3 className="text-lg font-semibold">
-                        {name}
-                    </h3>
-
-                    <p className="font-bold">
-                        Price: <span className="text-orange-500 font-bold">${price}</span>
-                    </p>
-
-                    <div className="flex items-center gap-2 text-sm">
-                        <FaStar className="text-yellow-500" />
-                        {rating}
-                    </div>
-
+                <Link href={`/products/${id}`}>
                     <Button className="mt-3 w-full">
                         View Details
                     </Button>
+                </Link>
+            </div>
 
-                </div>
-            </Card>
-        </div>
+        </Card>
     );
 };
 
